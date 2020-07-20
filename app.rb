020413ed4b1e7d47b7aa9d37cdd10372109fbe84
jonah <benjaminjmallard@gmail.com>
2020-07-20 18:20:54 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-    
+    # new action
     get '/newteam' do
         erb :newteam
     end
-    
+    # create action
     post '/team' do
         puts params
         erb :team
